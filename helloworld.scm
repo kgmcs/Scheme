44db88f87a41adf379e31ae4fe14395352124eab
kgmcs <84094991+kgmcs@users.noreply.github.com>
2021-05-12 20:08:16 -0400

    Update helloworld.scm

--- helloworld.scm
+++ helloworld.scm
@@ -1,2 +1,10 @@
+;; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+;;        Name: Kyla Garcia McShane
+;;   Course Info: CSC153 - Spring 2021
+;;   Description: Final Project Explorations
+;; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+;; Although this program is in a zip file, I strongly advice (require) running it on replit since it was made using BiwaScheme Interpreter version 0.6.4 which is not what most Scheme interpreters use.
+
 (display "Hello, World!")
 (newline)
