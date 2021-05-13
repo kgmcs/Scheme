6a6c973f6b51fa87ffe42873447b79a8c880962c
kgmcs <84094991+kgmcs@users.noreply.github.com>
2021-05-12 20:29:43 -0400

    Update helloworld.scm

--- helloworld.scm
+++ helloworld.scm
@@ -6,6 +6,7 @@
 
 ;; Although this program is in a zip file, I strongly advice (require) running it on replit since it was made using BiwaScheme Interpreter version 0.6.4 which is not what most Scheme interpreters use.
 ;; Link to run on replit: https://replit.com/@KylaGarcia/helloworld
+;; Replit edit link if first link does not work: https://replit.com/join/fsonehhh-kylagarcia
 
 (display "Hello, World!")
 (newline)
